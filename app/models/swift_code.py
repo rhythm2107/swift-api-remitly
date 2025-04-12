bbe8951ef892d52cc2ca3ea9d61c030624830873
rhythm2107 <kamil.krzywda.21@gmail.com>
2025-04-12 20:47:57 +0200

    Add SwiftCode model with constraints for ISO2

---
+++ app/models/swift_code.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, String, Boolean, Integer, CheckConstraint
+from app.core.database import Base
+
+class SwiftCode(Base):
+    __tablename__ = "swiftcode"
+
+    id = Column(Integer, primary_key=True, index=True)
+    swift_code = Column(String(11), unique=True, nullable=False)
+    bank_name = Column(String(255), nullable=False)
+    address = Column(String(255), nullable=True)
+    country_iso2 = Column(String(2), nullable=False)
+    country_name = Column(String(100), nullable=False)
+    is_headquarter = Column(Boolean, default=False)
+
+    # Ensure that ISO2 is always of length 2
+    __table_args__ = (
+        CheckConstraint("char_length(country_iso2) = 2", name="check_country_iso2_len")
+    )
