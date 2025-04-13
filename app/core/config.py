507a56320ac6295a58c14ae211ec8c4e22bcc2cb
rhythm2107 <kamil.krzywda.21@gmail.com>
2025-04-13 21:54:57 +0200

    Expand settings with new env vars and add SWIFT code CSV/XLSX files

--- app/core/config.py
+++ app/core/config.py
@@ -2,11 +2,13 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     DATABASE_URL: str
+    INPUT_SOURCE: str
+    XLSX_FILE_PATH: str
+    CSV_FILE_PATH: str
+    GOOGLE_SHEET_URL: str
 
     class Config:
         env_file = ".env"
         extra = "ignore"
 
 settings = Settings()
-
-print(settings)
