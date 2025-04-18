8d0bed81560b69e0045c0817b150a96993da741f
rhythm2107 <kamil.krzywda.21@gmail.com>
2025-04-18 17:20:06 +0200

    Prevent entrypoint from exiting if seed_data.py was already run

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,9 @@ echo "Running Alembic migrations..."
 poetry run alembic upgrade head
 
 echo "Seeding the database..."
-poetry run python app/ingestion/seed_data.py
+poetry run python app/ingestion/seed_data.py \
+  && echo "Seed completed" \
+  || echo "Seed was already done in previous iteration, continuing."
 
 echo "Starting the API server..."
 exec poetry run uvicorn app.main:app --host 0.0.0.0 --port 8080
