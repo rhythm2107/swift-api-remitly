35a9fa11f2fa7308aab69b891461dc6e15c6396a
rhythm2107 <kamil.krzywda.21@gmail.com>
2025-04-18 14:54:11 +0200

    Move FastAPI initialization to create_app() for testing flexibility

--- app/main.py
+++ app/main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from app.api.v1 import swift_codes
+from app.ingestion.seed_data import seed_data
+from app.core.config import settings
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    yield
+def create_app() -> FastAPI:
+    @asynccontextmanager
+    async def lifespan(app: FastAPI):
+        if settings.DEV_MODE.lower() == "false":
+            print("Seeding data...")
+            await seed_data()
 
-app = FastAPI(lifespan=lifespan)
-app.include_router(swift_codes.router, prefix="/v1/swift-codes", tags=["swift-codes"])
+        if settings.DEV_MODE.lower() == "true":
+            print("Skipping seed_data() for test environment")
+
+        yield
+
+    app = FastAPI(lifespan=lifespan)
+    app.include_router(swift_codes.router, prefix="/v1/swift-codes", tags=["swift-codes"])
+    return app
+
+# For Uvicorn
+app = create_app()
